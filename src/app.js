097d9779ce512ea293d9993a107c264ee6514f65
sepuckett86 <sepuckett86@gmail.com>
2019-04-26 14:32:26 -0700

    add to app.js to get word section from DOM and add divs with addLetterDivs function

--- src/app.js
+++ src/app.js
@@ -1,1 +1,7 @@
 import words from './words.js';
+import generateRandom from './generateRandom.js';
+import wordGenerator from './wordGenerator.js';
+import addLetterDivs from './addLetterDivs.js';
+
+const wordSection = document.getElementById('word');
+addLetterDivs('dummy', wordSection);
